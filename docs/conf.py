407dcdfe8822ec3e5b211cdc2477a27b0054b33c
007 <007gzs@gmail.com>
2019-11-06 17:40:27 +0800

    pep8

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath('..'))
 
-import allinpay
+import allinpay  # NOQA
 
 
 # -- Project information -----------------------------------------------------
