23ed31c7aec20bba38bc4cd585ddb13226be4c4b
007 <007gzs@gmail.com>
2019-11-19 15:28:51 +0800

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pyallinpay',
-    version='1.1.0',
+    version='1.1.1',
     keywords='pyallinpay, SDK, 通联支付',
     description='AllInPay SDK for Python',
     long_description=long_description,
